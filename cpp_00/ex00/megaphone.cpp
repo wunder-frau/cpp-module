0c27213222322c292c16e204bb7a5fd9121be875
irene <wunder.frau@yandex.by>
2024-11-03 20:04:57 +0200

    feature/cpp00: megaphone

--- cpp_00/ex00/megaphone.cpp
+++ cpp_00/ex00/megaphone.cpp
@@ -1,10 +1,19 @@
 #include <iostream>
-// #include <string_view>
 
-int main() {
-    using namespace std::string_view_literals;
-
-    std::cout << "Hello, World!"sv << std::endl;
-
+int main(int argc, char **argv) {
+    (void)argv;
+    if (argc == 1)
+        std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+    else {
+        for (int i = 1; i < argc; i++) {
+            for (int j = 0; argv[i][j] != '\0'; j++) {
+                std::cout << static_cast<char>(std::toupper(argv[i][j]));
+            }
+            if (i < argc - 1) {
+                std::cout << " ";
+            }
+        }
+        std::cout << std::endl;
+    }
     return 0;
 }
