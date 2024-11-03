407c6b6f90b0bbe1f69d3f7b034cd1239d1dbb06
irene <wunder.frau@yandex.by>
2024-11-03 19:48:28 +0200

    feature/cpp00: add Makefile aand  output

---
+++ cpp_00/ex00/megaphone.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+// #include <string_view>
+
+int main() {
+    using namespace std::string_view_literals;
+
+    std::cout << "Hello, World!"sv << std::endl;
+
+    return 0;
+}
