8bd416d0737061e63272ef9864e4fe2ba700231d
irene <wunder.frau@yandex.by>
2024-11-26 00:15:40 +0200

    ex00

--- cpp_00/ex00/megaphone.cpp
+++ cpp_00/ex00/megaphone.cpp
@@ -26,36 +26,3 @@ int main(int argc, char **argv) {
 
     return 0;
 }
-
-// #include <iostream>
-// #include <string>
-
-// void ToUpper(string &text) {
-//     for (char &c : std::string(argv[i])) {
-//         // (void)c;
-//         std::cout << static_cast<char>(std::toupper(c));
-//     }
-// }
-
-// int main(int argc, char **argv) {
-//     if (argc == 1) {
-//         std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
-
-//         return 0;
-//     }
-//     int i = 1;
-//     for (; i < argc; i++) {
-//         // for (int j = 0; argv[i][j] != '\0'; j++) {
-//         for (char &c : std::string(argv[i])) {
-//             // (void)c;
-//             std::cout << static_cast<char>(std::toupper(c));
-//         }
-//     }
-//     if (i < argc - 1) {
-//         std::cout << " ";
-//     }
-
-//     std::cout << std::endl;
-
-//     return 0;
-// }
