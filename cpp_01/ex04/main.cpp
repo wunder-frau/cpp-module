7c9536c1e54ef085a0626ea545f6143f858ae8b3
irene <wunder.frau@yandex.by>
2024-11-27 00:52:27 +0200

    cpp_01/ex04: error handling in main

--- cpp_01/ex04/main.cpp
+++ cpp_01/ex04/main.cpp
@@ -1,7 +1,25 @@
+#include <iostream>
+#include <string>
+
 #include "SedForLosers.hpp"
 
-int main() {
-    SedForLosers sed("lol.txt", "hello", "goodbye");
-    sed.process();
+int main(int argc, char **argv) {
+    if (argc != 4) {
+        std::cerr << "Usage: ./sed <filename> <string1> <string2>" << std::endl;
+        return 1;
+    }
+
+    std::string filename = argv[1];
+    std::string s1 = argv[2];
+    std::string s2 = argv[3];
+
+    SedForLosers sed(filename, s1, s2);
+
+    if (sed.process()) {
+        std::cout << "File processed successfully! Replaced text is in "
+                  << filename << ".replace." << std::endl;
+    } else {
+        std::cerr << "Error processing file." << std::endl;
+    }
     return 0;
 }
