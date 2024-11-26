95d6f8b34d68e264950dd30c0163f12f5aeef4d5
irene <wunder.frau@yandex.by>
2024-11-26 09:44:44 +0200

    PhoneBook.hpp rearangement

--- cpp_00/ex01/PhoneBook.hpp
+++ cpp_00/ex01/PhoneBook.hpp
@@ -7,27 +7,34 @@
 
 class PhoneBook {
 public:
+    // Constructor
     PhoneBook() : contact_count_(0), oldest_index_(0) {};
 
+    // Public interface methods
     bool run(const std::string& cmd);
-    void addContact(const Contact& newContact);
     void searchContacts() const;
     void displayContact(int index) const;
+
+private:
+    // Core phone book functionality
+    void addNewContact();
+    void addContact(const Contact& newContact);
+    void searchForContact();
+
+    // Display utilities
     void displayHeader() const;
     void displayContactRow(int index) const;
     void truncateField(std::string& field) const;
 
-private:
-    Contact contacts_[8];
-    int contact_count_;
-    int oldest_index_;
-
+    // Validation and input utilities
+    std::string getValidName(const std::string& prompt) const;
+    std::string getValidPhoneNumber() const;
+    std::string getValidDarkestSecret() const;
     bool isValidName(const std::string& name, std::string& errorMsg) const;
     bool isValidPhoneNumber(const std::string& number,
                             std::string& errorMsg) const;
-    std::string getValidDarkestSecret() const;
-    std::string getValidName(const std::string& prompt) const;
-    std::string getValidPhoneNumber() const;
-    void addNewContact();
-    void searchForContact();
+
+    Contact contacts_[8];
+    int contact_count_;
+    int oldest_index_;
 };
