18a2c5f71d883178b8f9a86670080fba9c150082
irene <wunder.frau@yandex.by>
2024-12-12 15:32:59 +0200

    cpp00/ex01: add error message if there are args passed to program

--- cpp_00/ex01/main.cpp
+++ cpp_00/ex01/main.cpp
@@ -2,7 +2,11 @@
 #include <string>
 
 #include "PhoneBook.hpp"
-int main() {
+int main(int argc, char **argv) {
+    if (argc > 1) {
+        std::cout << "This program doesn't require arguments. Remove this: "
+                  << argv[1] << ". Run: ./phonebook" << std::endl;
+    }
     PhoneBook phonebook;
     phonebook.run();
     return 0;
