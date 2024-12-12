8cc08e16c7c1dbf98e9a6545208d480fbe2ba48e
irene <wunder.frau@yandex.by>
2024-12-12 15:35:39 +0200

    add return

--- cpp_00/ex01/main.cpp
+++ cpp_00/ex01/main.cpp
@@ -6,6 +6,7 @@ int main(int argc, char **argv) {
     if (argc > 1) {
         std::cout << "This program doesn't require arguments. Remove this: "
                   << argv[1] << ". Run: ./phonebook" << std::endl;
+        return 0;
     }
     PhoneBook phonebook;
     phonebook.run();
