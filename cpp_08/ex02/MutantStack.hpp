ff9383f42c61a7e62ef062b2c71fdf4bd63322b0
irene <wunder.frau@yandex.by>
2025-05-23 11:08:44 +0300

    refactor(ex02): move MutantStack template method definitions to separate .tpp file

--- cpp_08/ex02/MutantStack.hpp
+++ cpp_08/ex02/MutantStack.hpp
@@ -6,28 +6,25 @@
 template <typename T, typename Container = std::deque<T> >
 class MutantStack : public std::stack<T, Container> {
 public:
-    // Required typedefs
     using iterator = typename Container::iterator;
     using const_iterator = typename Container::const_iterator;
     using reverse_iterator = typename Container::reverse_iterator;
     using const_reverse_iterator = typename Container::const_reverse_iterator;
 
-    // Constructors & assignment
-    MutantStack() = default;
-    MutantStack(const MutantStack& other) = default;
-    MutantStack& operator=(const MutantStack& other) = default;
-    ~MutantStack() = default;
+    MutantStack();
+    MutantStack(const MutantStack& other);
+    MutantStack& operator=(const MutantStack& other);
+    ~MutantStack();
 
-    // Iterator access
-    iterator begin() { return this->c.begin(); }
-    iterator end() { return this->c.end(); }
+    iterator begin();
+    iterator end();
+    const_iterator begin() const;
+    const_iterator end() const;
 
-    const_iterator begin() const { return this->c.begin(); }
-    const_iterator end() const { return this->c.end(); }
-
-    reverse_iterator rbegin() { return this->c.rbegin(); }
-    reverse_iterator rend() { return this->c.rend(); }
-
-    const_reverse_iterator rbegin() const { return this->c.rbegin(); }
-    const_reverse_iterator rend() const { return this->c.rend(); }
+    reverse_iterator rbegin();
+    reverse_iterator rend();
+    const_reverse_iterator rbegin() const;
+    const_reverse_iterator rend() const;
 };
+
+#include "MutantStack.tpp"
